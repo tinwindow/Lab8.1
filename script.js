5d3c0c5450c80f9494e074aed798c9372defe1cb
tinwindow <erin.ura@gmail.com>
2017-08-21 14:56:24 -0400

    final commit everything working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- $(document).ready(function(){   
+ $(document).ready(function(){
 
 var items = [
 	 {name: "Cat Food", price: 17.50},
@@ -7,38 +7,38 @@ var items = [
 	 {name: "Paper Towels", price: 1.79},
      {name: "Star Trek Pizza Cutter", price: 20.00}
 ];
- 
-	
+
+
 var $total = 0;
-var $el = $("<Grocery List>");
+var $orderedList = $("#GroceryList");
 
 for (var i = 0; i < items.length; i++) {
   console.log(items[i].name, items[i].price);
 
-  $total = $total = items[i].price;
- 
-var $el = $("<li>");
- $el.text ("<name>", "<price>");
- $("#GroceryList").append($el);
+  $total = $total + items[i].price;
+
+
+  var $listItem = $("<li>"); //here you're creating the element
+  $listItem.text (items[i].name + " $ " + items[i].price);
+  $("#GroceryList").append($listItem);//here you're appending the element to the GroceryList (an OL)
 
 }
 
- console.log("Your total is" + $total + ".");
-
- var $el = ("#total").append($el) = $total;
+ console.log("Your total is " + $total + ".");
 
 
- 
-
- 
-
- 
-
-
-}); //end ready !!!!! 
+  $("#total").append($total) = $total; //just appending it because it's already been created!
 
 
 
 
 
- 
+
+
+
+
+
+
+
+
+}); //end ready !!!!!
